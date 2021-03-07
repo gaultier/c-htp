0293633ce600bdc26f95ef608579e4ca85d3657a
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-03-07 09:23:57 +0100

    [wip]

--- gitlab-api.c
+++ gitlab-api.c
@@ -15,6 +15,14 @@
 typedef int64_t i64;
 typedef uint64_t u64;
 
+static int json_eq(const char *json, jsmntok_t *tok, const char *s) {
+  if (tok->type == JSMN_STRING && (int)strlen(s) == tok->end - tok->start &&
+      strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
+    return 0;
+  }
+  return -1;
+}
+
 typedef struct {
   i64 pf_id;
   sds pf_name;
@@ -35,39 +43,46 @@ void project_init(project_t *project, i64 id) {
 }
 
 void project_parse_json(project_t *project) {
-  jsmntok_t *tokens = NULL;
+  jsmntok_t *tokens = calloc(sdslen(project->pf_api_data), sizeof(jsmntok_t));
+  assert(tokens);
+
   jsmn_parser parser;
   jsmn_init(&parser);
 
-  int res = jsmn_parse(&parser, project->pf_api_data,
-                       sdslen(project->pf_api_data), NULL, 0);
-  if (res <= 0) {
-    fprintf(stderr, "%s:%d:Failed to parse project info: id=%lld res=%d\n",
-            __FILE__, __LINE__, project->pf_id, res);
+  const char *const s = project->pf_api_data;
+  int res = jsmn_parse(&parser, s, sdslen((char *)s), NULL, 0);
+  if (res <= 0 || tokens[0].type != JSMN_OBJECT) {
+    fprintf(stderr,
+            "%s:%d:Malformed JSON for project: id=%lld res=%d data=`%s`\n",
+            __FILE__, __LINE__, project->pf_id, res, s);
     goto end;
   }
+  printf("JSON project: id=%lld tokens=%lld data=`%s`\n", project->pf_id, res,
+         s);
 
-  tokens = calloc(res, sizeof(jsmntok_t));
-  assert(tokens);
+  for (i64 i = 0; i < res; i++)
+    printf("[%lld] token=%d\n", project->pf_id, tokens[i].type);
 
-  jsmn_parse(&parser, project->pf_api_data, sdslen(project->pf_api_data),
-             tokens, res);
-  if (tokens[0].type != JSMN_OBJECT) {
-    fprintf(stderr, "%s:%d:Malformed JSON for project: id=%lld\n", __FILE__,
-            __LINE__, project->pf_id);
-    goto end;
+  for (i64 i = 1; i < res; i++) {
+    jsmntok_t *tok = &tokens[i];
+    if (tok->type != JSMN_STRING) continue;
+
+    const u64 len = tok->end - tok->start;
+    if (json_eq(s, tok, "name") == 0) {
+      project->pf_name = sdscatlen(project->pf_name, s + tok->start, len);
+    } else if (json_eq(s, tok, "path_with_namespace") == 0) {
+      project->pf_path_with_namespace =
+          sdscatlen(project->pf_path_with_namespace, s + tok->start, len);
+    }
   }
-
-  printf("JSON project: id=%lld tokens=%lld\n", project->pf_id, res);
 
 end:
-  if (tokens) free(tokens);
+  free(tokens);
 }
 
 static size_t write_cb(char *data, size_t n, size_t l, void *userp) {
   const i64 project_i = (i64)userp;
   project_t *project = &projects[project_i];
-  fprintf(stderr, "[%s] %.*s\n", project->pf_api_url, (int)(n * l), data);
   project->pf_api_data = sdscatlen(project->pf_api_data, data, n * l);
 
   return n * l;
@@ -116,6 +131,9 @@ int main() {
                 curl_easy_strerror(msg->data.result), project->pf_api_url);
         if (msg->data.result == 0) {
           project_parse_json(project);
+          printf("Project: id=%lld path_with_namespace=%s name=%s\n",
+                 project->pf_id, project->pf_path_with_namespace,
+                 project->pf_name);
         }
         curl_multi_remove_handle(cm, e);
         curl_easy_cleanup(e);
